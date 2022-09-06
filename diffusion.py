ad30a61b38116faa13ca7b8331222ca272d02c6c
barnden <brandon@nguyen.vc>
2022-09-05 20:13:13 -0500

    Cleanup code

--- diffusion.py
+++ diffusion.py
@@ -9,7 +9,7 @@ import os
 from model import Net
 
 
-def generate_schedule(steps, initial=1e-4, final=2e-2):
+def generate_constants(steps, initial=1e-4, final=2e-2):
     """
     Generates a linear schedule from [initial, final].
     Returns (alpha, beta), where beta is the linear schedule and alpha is 1 - beta
@@ -18,20 +18,37 @@ def generate_schedule(steps, initial=1e-4, final=2e-2):
     beta = torch.linspace(initial, final, steps)
     alpha = 1 - beta
 
-    return (alpha, beta)
+    sqrt_alpha = torch.sqrt(alpha)
+
+    # See Eq. 4 (DDPM) for formulation of alpha bar
+    alpha_bar = torch.cumprod(alpha, dim=0)
+    sqrt_alpha_bar = torch.sqrt(alpha_bar)
+    sqrt_one_minus_alpha_bar = torch.sqrt(1.0 - alpha_bar)
+
+    # See Eq. 6 (DDPM) for formulation of beta tilde
+    alpha_bar_prev = torch.cat((torch.tensor([1]), alpha_bar[:-1]))
+    beta_tilde = beta * (1 - alpha_bar_prev) / (1 - alpha_bar)
+
+    return (
+        alpha,
+        beta,
+        sqrt_alpha,
+        alpha_bar,
+        sqrt_alpha_bar,
+        sqrt_one_minus_alpha_bar,
+        beta_tilde,
+    )
 
 
 def get_index_from_list(vals, t, x_shape):
-    """
-    Taken from DeepFindr's video (see README)
-    """
+    # Helper function taken from DeepFindr's video (see README)
     batch_size = t.shape[0]
     out = vals.gather(-1, t.cpu())
 
     return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
 
 
-def forward(x_0, t, device="cuda"):
+def forward(x_0, t):
     """
     Generates a noised image :math:`x_t` given an :math:`x_0`.
 
@@ -42,14 +59,13 @@ def forward(x_0, t, device="cuda"):
 
     :param x_0: torch.Tensor: A 4D batch tensor (B, C, W, H)
     :param t: torch.Tensor: A 1D tensor of time steps to generate
-    :param device: str: Either "cpu" or "cuda"
 
     :return: (xt, E), A tuple where xt is the result of the forward process after t, and E is the noise applied onto the image.
     :rtype: (torch.Tensor, torch.Tensor)
     """
 
-    A_t = get_index_from_list(alpha_bar_sqrt, t, x_0.shape).to(device)
-    stdev = get_index_from_list(one_minus_alpha_bar_sqrt, t, x_0.shape).to(device)
+    A_t = get_index_from_list(sqrt_alpha_bar, t, x_0.shape).to(device)
+    stdev = get_index_from_list(sqrt_one_minus_alpha_bar, t, x_0.shape).to(device)
 
     E = torch.randn_like(x_0).to(device)
     x_t = (x_0.to(device) * A_t) + (stdev * E)
@@ -58,28 +74,12 @@ def forward(x_0, t, device="cuda"):
 
 
 def L2Loss(model, x_0, t):
+    # Find L2 distance between predicted epsilon and actual epsilon.
+
     x_t, E = forward(x_0, t, device)
     predicted_E = model(x_t, t)
 
     return F.mse_loss(predicted_E, E)
-
-
-@torch.no_grad()
-def sample_timestep(x, t):
-    b_t = get_index_from_list(beta, t, x.shape)
-    somact = get_index_from_list(one_minus_alpha_bar_sqrt, t, x.shape)
-    recipr = get_index_from_list(alpha_bar_recip_sqrt, t, x.shape)
-
-    model_mean = recipr * (x - b_t * model(x, t) / somact)
-
-    if t == 0:
-        return model_mean
-
-    posterior_variance_t = get_index_from_list(posterior_variance, t, x.shape)
-    noise = torch.randn_like(x)
-    mean = model_mean + torch.sqrt(posterior_variance_t) * noise
-
-    return mean
 
 
 def create_dataloader(image_size=(256, 256), batch_size=5):
@@ -112,7 +112,7 @@ def create_dataloader(image_size=(256, 256), batch_size=5):
 @torch.no_grad()
 def test_forward_process(dataloader):
     """
-    Generate x_t for t=0 to t=300 by increments of 10.
+    Generate x_t for t=0 to t=nsteps by increments of 10.
     Used to debug the forward process.
     """
 
@@ -121,7 +121,7 @@ def test_forward_process(dataloader):
 
     images = []
 
-    for idx in range(0, 300, 10):
+    for idx in range(0, nsteps, 10):
         t = torch.Tensor([idx]).type(torch.int64)
         xt, _ = forward(image, t)
         images.append(xt.detach().cpu())
@@ -134,16 +134,19 @@ def test_forward_process(dataloader):
 
 
 @torch.no_grad()
-def test_model(dataloader=None):
+def generate(dataloader=None):
     """
-    Generate an x_T, then use reverse diffusion model to find x_0
+    Generate an x_T, then use reverse diffusion model to find x_0.
+
+    If a dataloader is given, then we take the first image as x_0.
+    This x_0 is passed into the forward process to generate x_T,
+    which is then denoised into a predicted x'_0.
     """
 
     images = []
     if dataloader != None:
         image = next(iter(dataloader))[0][0]
         image = torch.squeeze(image)
-
         step = 60
 
         for idx in range(0, 300, step):
@@ -154,9 +157,8 @@ def test_model(dataloader=None):
         x = images[-1][None, ...].to(device)
     else:
         image = torch.randn((3, image_size, image_size)).to(device)
-        x = image[None, ...]
         step = 30
-
+        x = image[None, ...]
 
     for idx in reversed(range(300)):
         t = torch.Tensor([idx]).type(torch.int64).to(device)
@@ -174,40 +176,54 @@ def test_model(dataloader=None):
 
 @torch.no_grad()
 def denoise(x, t):
-    betas_t = get_index_from_list(beta, t, x.shape)
-    somact = get_index_from_list(one_minus_alpha_bar_sqrt, t, x.shape)
-    srat = get_index_from_list(alpha_bar_recip_sqrt, t, x.shape)
+    """
+    Given an image :math:`x_t`, denoise the image using the model to find :math:`x_{t - 1}`.
+
+    From Eq. 10 in DDPM:
+
+    .. math::
+        \\mu_\\theta = \\frac{1}{\\sqrt{\\bar{\\alpha_t}}}\\left(x_t - \\frac{\\beta_t}{\\sqrt{1 - \\bar{\\alpha}_t}}\\varepsilon\\right)
+
+    Using :math:`\\mu_\\theta`, we can then reconstruct :math:`x_{t - 1}`.
+    """
+    beta_t = get_index_from_list(beta, t, x.shape)
+    sqrt_one_minus_alpha_bar_t = get_index_from_list(
+        sqrt_one_minus_alpha_bar, t, x.shape
+    )
+    alpha_sqrt_t = get_index_from_list(sqrt_alpha, t, x.shape)
 
     E_theta = model(x, t)
-    mdl = srat * (x - betas_t * E_theta / somact)
+    mu_theta = (x - beta_t * E_theta / sqrt_one_minus_alpha_bar_t) / alpha_sqrt_t
 
     if t == 0:
-        return mdl
+        return mu_theta
 
     noise = torch.randn_like(x)
-    b_t1 = get_index_from_list(posterior_variance, t, x.shape)
-    den = mdl + torch.sqrt(b_t1) * noise
+    beta_tilde_t = get_index_from_list(beta_tilde, t, x.shape)
+    predicted = mu_theta + torch.sqrt(beta_tilde_t) * noise
 
-    return den
+    return predicted
 
 
 if __name__ == "__main__":
     batch_size = 32
     image_size = 64
-
     nsteps = 300
-    alpha, beta = generate_schedule(steps=nsteps)
-    alpha_bar = torch.cumprod(alpha, dim=0)
-    alpha_bar_sqrt = torch.sqrt(alpha_bar)
-    one_minus_alpha_bar_sqrt = torch.sqrt(1.0 - alpha_bar)
-    alpha_bar_recip_sqrt = torch.sqrt(1.0 / alpha)
-    alpha_bar_prev = F.pad(alpha_bar[:-1], (1, 0), value=1.0)
-    posterior_variance = beta * (1 - alpha_bar_prev) / (1 - alpha_bar)
 
+    (
+        alpha,
+        beta,
+        sqrt_alpha,
+        alpha_bar,
+        sqrt_alpha_bar,
+        sqrt_one_minus_alpha_bar,
+        beta_tilde,
+    ) = generate_constants(steps=nsteps)
+
+    # Create/load model
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = Net(image_size).to(device)
 
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
     dataloader = create_dataloader((image_size, image_size), batch_size)
 
     if os.path.exists("model.pth"):
@@ -219,9 +235,11 @@ if __name__ == "__main__":
 
     # Set to True to generate an image from random noise
     if True:
-        for x in range(25):
-            test_model()
+        # Optional: Pass dataloader to take a random image, add noise, then reverse noise to generate a nearest neighbour image
+        generate()
     else:
+        optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
         for epoch in range(0, 100):
             print("-" * 32)
             print(f"Epoch {epoch + 1}")
