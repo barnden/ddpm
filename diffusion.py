cc4262b224ff2c5e9de1cf94e2a776a1f50ef8d6
barnden <brandon@nguyen.vc>
2022-09-07 18:44:45 -0500

    Disable training if --generate flag is passed

--- diffusion.py
+++ diffusion.py
@@ -304,7 +304,7 @@ if __name__ == "__main__":
             else:
                 generate(dataloader)
 
-    if not args.disable_dataloader:
+    if not args.disable_dataloader and not args.generate:
         optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
         for epoch in range(args.epochs):
